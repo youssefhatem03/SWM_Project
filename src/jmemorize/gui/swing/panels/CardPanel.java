80d48649e86115c04591f79c50a69c6baae13001
NourrAdel <nour.adel.mahmoud1@gmail.com>
2024-05-07 22:46:59 +0300

    Updated Function

--- src/jmemorize/gui/swing/panels/CardPanel.java
+++ src/jmemorize/gui/swing/panels/CardPanel.java
@@ -258,7 +258,27 @@ public class CardPanel extends JPanel
             StyleConstants.setBold(attr, enabled);
         }
     }
-    
+
+
+    private class StrikeThruAction extends AbstractStyledTextAction
+    {
+        public StrikeThruAction()
+        {
+            super("font-strike-through");
+            addShortcut(KeyStroke.getKeyStroke(KeyEvent.VK_B, CTRL_MASK));
+        }
+
+        public boolean hasStyle(AttributeSet attr)
+        {
+            return StyleConstants.isStrikeThrough(attr);
+        }
+
+        public void setStyle(MutableAttributeSet attr, boolean enabled)
+        {
+            StyleConstants.setStrikeThrough(attr, enabled);
+        }
+    }
+
     private class ItalicAction extends AbstractStyledTextAction
     {
         public ItalicAction()
